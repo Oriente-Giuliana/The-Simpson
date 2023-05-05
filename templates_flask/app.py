33509d4eccb68993080e003780acc4a40a42d26d
Oriente-Giuliana <oriente.giuliana@iisgalvanimi.edu.it>
2023-05-05 09:50:54 +0000

    ciao

--- templates_flask/app.py
+++ templates_flask/app.py
@@ -1,10 +1,21 @@
 from flask import render_template
 from flask import Flask
+import mysql.connector
 
 
 app = Flask(__name__)
 
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="THE_SIMPSON"
+)
 
-@app.route('/')
-def hello():
-    return render_template('hello.html', name='Fabio')
+mycursor = mydb.cursor()
+
+@app.route('/units')
+def personaggiList():
+    mycurson.execute("SELECT * FROM THE_SIMPSON")
+    myresult=mycursor.fetchall()
+    return render_template('the_simpson.html', units=myresult)
