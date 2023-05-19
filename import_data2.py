597b8ef6605ab15076a92e75acf15c897721895f
Oriente-Giuliana <oriente.giuliana@iisgalvanimi.edu.it>
2023-05-19 09:49:29 +0000

    versione1

--- import_data2.py
+++ import_data2.py
@@ -14,21 +14,22 @@ mycursor = mydb.cursor()
 mycursor.execute("CREATE DATABASE IF NOT EXISTS THE_SIMPSON")
 
 #Create the table for the csv data (if not exists)
+
 mycursor.execute("""
-  CREATE TABLE IF NOT EXISTS THE_SIMPSON.personaggi(
-    nome VARCHAR(30),
-    cognome VARCHAR(30),
-    genere VARCHAR(30),
+  CREATE TABLE IF NOT EXISTS THE_SIMPSON.doppiatori(
+    `DOPPIATORE ORIGINALE` VARCHAR(30),
+    `DOPPIATORE ITALIANO` VARCHAR(30),
     PRIMARY KEY (Name)
   );""")
 
 
+
 #Delete data from the table personaggi
-mycursor.execute("DELETE FROM THE_SIMPSON.personaggi")
+mycursor.execute("DELETE FROM THE_SIMPSON.doppiatori")
 mydb.commit()
 
 #Read data from a csv file
-Simpsons_data = pd.read_csv('./simpsons_characters.csv', index_col=False, delimiter = ',')
+Simpsons_data = pd.read_csv('./simpsons_doppiatori.csv', index_col=False, delimiter = ',')
 Simpsons_data = Simpsons_data.fillna('Null')
 print(Simpsons_data.head(20))
 
@@ -37,14 +38,14 @@ print(Simpsons_data.head(20))
 for i,row in Simpsons_data.iterrows():
     cursor = mydb.cursor()
     #here %S means string values 
-    sql = "INSERT INTO THE_SIMPSON.personaggi VALUES (%s,%s,%s)"
+    sql = "INSERT INTO THE_SIMPSON.doppiatori VALUES (%s,%s)"
     cursor.execute(sql, tuple(row))
     print("Record inserted")
     # the connection is not auto committed by default, so we must commit to save our changes
     mydb.commit()
 
 #Check if the table has been filled
-mycursor.execute("SELECT * FROM THE_SIMPSON.personaggi")
+mycursor.execute("SELECT * FROM THE_SIMPSON.doppiatori")
 myresult = mycursor.fetchall()
 
 for x in myresult:
